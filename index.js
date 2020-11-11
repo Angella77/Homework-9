86507e9bbd7b9c1d6d3d98f5b1798ecede4c77de
Angella <angiejeanlewis@gmail.com>
2020-11-10 22:32:10 -0500

    add file

---
+++ index.js
@@ -0,0 +1,8 @@
+// need the const fs variable here
+const fs = require("fs");
+
+// need inquirer variable here
+const inquirer = require("inquirer");
+
+// need a markdown js file here
+const generateMarkdown = require("./utils/generateMarkdown");
